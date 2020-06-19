7ea9754a259254f34dd348a0f5d0a2ab89f3203d
thekla-petro <thekla_petro@yahoo.com>
2020-06-19 14:25:09 +0200

    6 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -15,9 +21,17 @@ import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
 import { CustomCurrencyPipe } from './custom-currency.pipe';
 
+
+
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
@@ -38,6 +52,7 @@ import { CustomCurrencyPipe } from './custom-currency.pipe';
     LeftBarComponent,
     CustomCurrencyPipe
   ],
+
   bootstrap: [ AppComponent ],
   providers: [CartService]
 })
--- src/app/left-bar/left-bar.component.ts
+++ src/app/left-bar/left-bar.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-left-bar',
   templateUrl: './left-bar.component.html',
   styleUrls: ['./left-bar.component.css']
 })
-export class LeftBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class LeftBarComponent {
+  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+  constructor(private breakpointObserver : BreakpointObserver ) { }
 }
